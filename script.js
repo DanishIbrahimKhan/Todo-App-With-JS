dda2a7caa8f5751f444edffc07406a853dddeaba
danish-ibrahim-khan <143989917+danish-ibrahim-khan@users.noreply.github.com>
2024-06-11 17:18:20 +0530

    add todo list

--- script.js
+++ script.js
@@ -7,26 +7,35 @@ var todoListContainer = document.getElementsByTagName("div")[1];
 heading.textContent = "Todo List";
 input.placeholder = "add todoList";
 button.textContent = "add Todo";
-deleteAll.textContent = "delete all"
+deleteAll.textContent = "delete all";
 button.onclick = () => addTodo();
-deleteAll.onclick = () => {todoList = [] 
-    setLocalStorage("todosList",[])
-    render()};
+deleteAll.onclick = () => {
+  todoList = [];
+  setLocalStorage("todosList", []);
+  render();
+};
 
 headingContainer.appendChild(heading);
 headingContainer.appendChild(input);
 headingContainer.appendChild(button);
-headingContainer.appendChild(deleteAll)
-var todoList = JSON.parse(localStorage.getItem("todosList"));
+headingContainer.appendChild(deleteAll);
+var todoList =
+  JSON.parse(localStorage.getItem("todosList")) === "todosList"
+    ? []
+    : JSON.parse(localStorage.getItem("todosList"));
+console.log(JSON.parse(localStorage.getItem("todosList")) === "todosList");
 function addTodo() {
-    todoList.unshift(input.value);
-    localStorage.setItem("todosList", JSON.stringify(todoList))
-  input.value = "";
-  render();
+  if (input?.value?.trim() !== "") {
+    todoList.unshift(input.value.trim());
+    localStorage.setItem("todosList", JSON.stringify(todoList));
+    input.value = "";
+    render();
+  }
 }
 function render() {
   todoListContainer.innerHTML = "";
   todoList.map((value, index) => {
+    let todoItemsContainer = document.createElement("div");
     let p = document.createElement("p");
     let button = document.createElement("button");
     let updateButton = document.createElement("button");
@@ -35,23 +44,24 @@ function render() {
     button.onclick = () => deleteTodo(index);
     updateButton.onclick = () => updateTodo(index);
     p.textContent = value;
-    todoListContainer.appendChild(p);
-    todoListContainer.appendChild(button);
-    todoListContainer.appendChild(updateButton);
+    todoItemsContainer.appendChild(p);
+    todoItemsContainer.appendChild(button);
+    todoItemsContainer.appendChild(updateButton);
+    todoListContainer.appendChild(todoItemsContainer);
   });
 }
 function deleteTodo(index) {
-    todoList.splice(index, 1);
-    setLocalStorage(todoList)
-    render();
-    }
-    function updateTodo(index) {
-        input.value = todoList[index];
-        todoList.splice(index, 1);
-        setLocalStorage(todoList);
-        render();
+  todoList.splice(index, 1);
+  setLocalStorage(todoList);
+  render();
 }
-function setLocalStorage(value){
-    localStorage.setItem("todosList",JSON.stringify(value))
+function updateTodo(index) {
+  input.value = todoList[index];
+  todoList.splice(index, 1);
+  setLocalStorage(todoList);
+  render();
+}
+function setLocalStorage(value) {
+  localStorage.setItem("todosList", JSON.stringify(value));
 }
 render();
